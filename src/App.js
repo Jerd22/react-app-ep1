7c0453d88197aee5161a01bde94ba7e7167b7ce0
Jerd22 <teeo300881@gmail.com>
2020-12-26 18:48:03 +0700

    EP.4 : React Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import  Home  from "./screens/Home";
+import  About  from "./screens/About";
+import  Users  from "./screens/Users";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link
+ } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Router path="/about">
+              <About />
+            </Router>
+            <Router path="/users">
+              <Users />
+            </Router>
+            <Router path="/">
+              <Home />
+            </Router>
+          </Switch>
+        </Router>
+        </header> 
     </div>
   );
 }
